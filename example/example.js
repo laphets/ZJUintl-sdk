48182a3b48edfb3f4128285fb3c4829ebe53f096
laphets <i@laphets.com>
2018-04-28 00:54:31 +0800

    Revise README

--- example/example.js
+++ example/example.js
@@ -1,10 +1,23 @@
 const ZJUintl = require('../src/zju_intl');
 
-
+// ZJUintl.connectTest('Laphets').then(res => {
+//     console.log(res);
+// })
 
 ZJUintl.getCourse({ username: '3170111705', password: '' }).then(res => {
     console.log(res);
 }).catch(err => {
     console.log(err);
-    })
+})
 
+ZJUintl.getBBGradeList({ username: '3170111705', password: '' }).then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+})
+
+ZJUintl.getBBCertainGrade({ username: '3170111705', password: '', courseid: '_2823_1' }).then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log(err);
+})
